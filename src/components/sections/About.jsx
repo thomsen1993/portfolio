f1ac0d74dcf72bb4dbf34ea7a39758fd3e2bfcd4
Benny Thomsen <benny.thomsen20@gmail.com>
2024-06-03 16:24:31 +0200

    button for more text

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 
 export const About = ({ data }) => {
+  const [more, setMore] = useState(2);
+
   return (
     <section className="col-span-3">
       <h2>{data.intro.title}</h2>
-      {data.intro.text.map(text => (
+      {data.intro.text.slice(0, more).map((text) => (
         <>
-          <p className="pb-3" key={text.id}>{text.paragraph}</p>
+          <p className="pb-3" key={text.id}>
+            {text.paragraph}
+          </p>
         </>
       ))}
+          {/* <button
+            className="w-full border border-borderColor rounded-b-md"
+            onClick={() => setMore(more === 2 ? 5 : 2)}
+          >
+            {more === 2 ? "Mais" : "Menos"}
+          </button> */}
     </section>
   );
 };
